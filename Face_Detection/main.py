536e772e5e2634a06022bbf69313f0879c479a1d
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-08 11:40:15 +0530

    Update main.py

--- Face_Detection/main.py
+++ Face_Detection/main.py
@@ -3,6 +3,9 @@ import cv2
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 img = cv2.imread('test.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+face = face_classifier.detectMultiScale(gray,1.1,4)
+
 
 
 cv2.imshow('img', img)
