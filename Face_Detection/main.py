cc4da3339a7f59eae3358edb3d9436a99f74a501
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-10 23:46:17 +0530

    Adding eye detection to the face detection model.

--- Face_Detection/main.py
+++ Face_Detection/main.py
@@ -11,7 +11,13 @@ while cap.isOpened():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face = face_classifier.detectMultiScale(gray, 1.1, 4)
 
-  
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_classifier.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 5 )
 
 
     cv2.imshow('img', img)
