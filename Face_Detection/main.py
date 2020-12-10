4c34e3bc6bb847dbcaffdc018d60f22cb72ed8f5
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-10 23:44:54 +0530

    Update main.py

--- Face_Detection/main.py
+++ Face_Detection/main.py
@@ -1,6 +1,7 @@
 import cv2
 
 face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
 cap = cv2.VideoCapture(0)
 
@@ -10,8 +11,8 @@ while cap.isOpened():
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     face = face_classifier.detectMultiScale(gray, 1.1, 4)
 
-    for (x, y, w, h) in face:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+  
+
 
     cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
