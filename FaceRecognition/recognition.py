540bbaf99b0227180c314f87d7195e4937ac38c2
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-12 16:36:14 +0530

    Update recognition.py

--- FaceRecognition/recognition.py
+++ FaceRecognition/recognition.py
@@ -1,7 +1,12 @@
 import os
+from PIL import  Image
+import numpy as np
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "Images")
+
+y_label = []
+x_train = []
 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
@@ -9,4 +14,10 @@ for root, dirs, files in os.walk(image_dir):
             path = os.path.join(root, file)
             label = os.path.basename(root).replace("","").lower()
             print(label, path)
-            
+            #y_label.append(label)
+            #x_train.append(path)
+            pil_image = Image.open(path).convert("L")#grayscale
+            image_array = np.array(pil_image, "uint8")
+            print(image_array)
+
+
