c1a1ba38b320b65a1d25517ff55eb1f29d5f7f30
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-12 16:20:55 +0530

    Update recognition.py

--- FaceRecognition/recognition.py
+++ FaceRecognition/recognition.py
@@ -1,3 +1,10 @@
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "Images")
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("jfif") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            print(path)
