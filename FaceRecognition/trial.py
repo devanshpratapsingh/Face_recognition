167db6373272af6af57eb48c19da086f8b2795ba
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-12 16:53:03 +0530

    Update trial.py

--- FaceRecognition/trial.py
+++ FaceRecognition/trial.py
@@ -1,14 +1,26 @@
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture(0)
+face_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_classifier = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
 
-while (True):
-    #Capture frame-by-frame
-    ret, frame = cap.read()
+cap = cv2.VideoCapture("testvid.mp4")
 
-    #Displaying the resulting frame
-    cv2.imshow('frame', frame)
+while cap.isOpened():
+    _, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    face = face_classifier.detectMultiScale(gray, 1.5, 5)
+
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_classifier.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew,ey+eh), (0,255,0), 5 )
+
+
+    cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
